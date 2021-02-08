a383554221724d0ee3a2330143f8e9344fa6ed56
Ana Sofia Castellanos <castellanos_chopas@hotmail.com>
2021-02-08 09:30:20 +0100

    added javascript

---
+++ js/product.js
@@ -0,0 +1,22 @@
+const url = "https://kea-alt-del.dk/t7/api/products/1165";
+//fetch data
+fetch(url)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    showProduct(data);
+  });
+
+//populate the page
+function showProduct(product) {
+  console.log(product);
+  document.querySelector(".breadcrumbs .category").textContent =
+    product.category;
+  document.querySelector(".breadcrumbs .productname").textContent =
+    product.productdisplayname;
+  document.querySelector(
+    "img.productimage"
+  ).src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+  document.querySelector("img.productimage").alt = product.id;
+}
