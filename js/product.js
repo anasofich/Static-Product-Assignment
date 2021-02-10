889d72837daedc542c9e6aeeeb9046a064211885
Ana Sofia Castellanos <castellanos_chopas@hotmail.com>
2021-02-10 22:17:09 +0100

    updated product parameters

--- js/product.js
+++ js/product.js
@@ -23,4 +23,20 @@ function showProduct(product) {
     "img.productimage"
   ).src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
   document.querySelector("img.productimage").alt = product.id;
+  document.querySelector("h2").textContent = product.productdisplayname;
+  document.querySelector(".price").textContent = product.price + " DKK";
+  document.querySelector(".hidden").textContent = product.price + " DKK";
+  if (product.soldout) {
+    document.querySelector("#productBox").classList.add("soldOut");
+  }
+
+  if (product.discount) {
+    document.querySelector("h3.hidden").classList.remove("hidden");
+    document.querySelector(".price").textContent =
+      product.price - product.price * (product.discount / 100) + " DKK";
+  }
+  document.querySelector(".brandName").textContent = product.brandname;
+  document.querySelector(".colorProduct").textContent = product.basecolour;
+  document.querySelector(".productionYear").textContent =
+    product.productionyear;
 }
